b9f1dfda5145e5a79d13f0cebf3ea0a6bcb46691
Jose Sanchez <jsanchezcastillejos@gmail.com>
2020-05-25 00:36:44 -0500

    Move only to calibrate script, needs some improvement yet

--- calibrator/calibrate.py
+++ calibrator/calibrate.py
@@ -1,81 +1,146 @@
+import sys
+
 import cv2
 import numpy as np
 
 
 class VideoCapture:
-    def __init__(self):
+    def __init__(self, noise=800):
         self.cap = cv2.VideoCapture(-1)
         self.cap.set(3, 1280)
         self.cap.set(4, 720)
 
-        cv2.namedWindow("Trackbars")
+        self.noise = noise
+        self.kernel = np.ones((5, 5), np.uint(8))
 
+    @staticmethod
+    def nothing(*args):
+        pass
+
+    def draw_canvas(self, canvas, x1, y1):
+        clear = False
+        wiper_thresh = 10000
+        penval = np.load('penval.npy')
+        _, frame = self.cap.read()
+        frame = cv2.flip(frame, 1)
+        if canvas is None:
+            canvas = np.zeros_like(frame)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_range = penval[0]
+        upper_range = penval[1]
+        mask = self.create_mask(hsv, lower_range, upper_range)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if contours and cv2.contourArea(max(contours, key=cv2.contourArea)) > self.noise:
+            co = max(contours, key=cv2.contourArea)
+            x2, y2, w, h = cv2.boundingRect(co)
+            area = cv2.contourArea(co)
+            print(area)
+            if 1000 < area < 4000 and (x1 != 0 or y1 != 0):
+                canvas = cv2.line(canvas, (x1, y1), (x2, y2), [255, 0, 0], 4)
+            x1, y1 = x2, y2
+            if area > wiper_thresh:
+                cv2.putText(canvas, 'Clearing Canvas', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5,
+                            cv2.LINE_AA)
+                clear = True
+        else:
+            x1, y1 = 0, 0
+
+        frame = cv2.add(frame, canvas)
+
+        stacked = np.hstack((canvas, frame))
+        cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.6, fy=0.6))
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            sys.exit(0)
+        # When c is pressed clear the canvas
+        if k == ord('c') or clear:
+            canvas = None
+        return canvas, x1, y1
+
+    def calibrator(self, draw_rectangle=False):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        frame = cv2.flip(frame, 1)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        l_h, l_s, l_v, u_h, u_s, u_v = self.calibrate()
+        lower_range = np.array([l_h, l_s, l_v])
+        upper_range = np.array([u_h, u_s, u_v])
+
+        mask = self.create_mask(hsv, lower_range, upper_range)
+
+        self.show_stack(frame, mask)
+        if draw_rectangle:
+            self.rectangle(mask, frame)
+
+        return VideoCapture.safe_to_pentval(l_h, l_s, l_v, u_h, u_s, u_v)
+
+    def create_mask(self, hsv, lower_range, upper_range):
+        mask = cv2.inRange(hsv, lower_range, upper_range)
+        mask = cv2.erode(mask, self.kernel, iterations=1)
+        mask = cv2.dilate(mask, self.kernel, iterations=2)
+        return mask
+
+    def rectangle(self, mask, frame):
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                               cv2.CHAIN_APPROX_SIMPLE)
+        if contours and cv2.contourArea(max(contours,
+                                            key=cv2.contourArea)) > self.noise:
+            c = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 25, 255), 2)
+        cv2.imshow('image', frame)
+        # ESC key
+        # if cv2.waitKey(5) & 0xFF == 27:
+        #     return True
+        # return False
+
+    @staticmethod
+    def calibrate():
+        cv2.namedWindow("Trackbars")
         cv2.createTrackbar("L - H", "Trackbars", 0, 179, VideoCapture.nothing)
         cv2.createTrackbar("L - S", "Trackbars", 0, 255, VideoCapture.nothing)
         cv2.createTrackbar("L - V", "Trackbars", 0, 255, VideoCapture.nothing)
         cv2.createTrackbar("U - H", "Trackbars", 179, 179, VideoCapture.nothing)
         cv2.createTrackbar("U - S", "Trackbars", 255, 255, VideoCapture.nothing)
         cv2.createTrackbar("U - V", "Trackbars", 255, 255, VideoCapture.nothing)
-
-    @staticmethod
-    def nothing(*args):
-        pass
-
-    def loop(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            return None
-        # Flip the frame horizontally (Not required)
-        frame = cv2.flip(frame, 1)
-
-        # Convert the BGR image to HSV image.
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # Get the new values of the trackbar in real time as the user changes them
         l_h = cv2.getTrackbarPos("L - H", "Trackbars")
         l_s = cv2.getTrackbarPos("L - S", "Trackbars")
         l_v = cv2.getTrackbarPos("L - V", "Trackbars")
         u_h = cv2.getTrackbarPos("U - H", "Trackbars")
         u_s = cv2.getTrackbarPos("U - S", "Trackbars")
         u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+        return l_h, l_s, l_v, u_h, u_s, u_v
 
-        # Set the lower and upper HSV range according to the value selected by the trackbar
-        lower_range = np.array([l_h, l_s, l_v])
-        upper_range = np.array([u_h, u_s, u_v])
-
-        # Filter the image and get the binary mask, where white represents your target color
-        mask = cv2.inRange(hsv, lower_range, upper_range)
-
-        # You can also visualize the real part of the target color (Optional)
-        res = cv2.bitwise_and(frame, frame, mask=mask)
-
-        # Converting the binary mask to 3 channel image, this is just so we can stack it with the others
-        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-        # stack the mask, orginal frame and the filtered result
-        stacked = np.hstack((mask_3, frame, res))
-
-        # Show this stacked frame at 40% of the size.
-        cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.4, fy=0.4))
-
-        # If the user presses ESC then exit the program
+    @staticmethod
+    def safe_to_pentval(l_h, l_s, l_v, u_h, u_s, u_v):
         key = cv2.waitKey(1)
-        if key == 27:
-            return None
-
-            # If the user presses `s` then print this array.
         if key == ord('s'):
-            thearray = [[l_h, l_s, l_v], [u_h, u_s, u_v]]
-            print(thearray)
+            config = [[l_h, l_s, l_v], [u_h, u_s, u_v]]
+            print(config)
+            np.save('penval', config)
+            cv2.destroyAllWindows()
+            return True
+        return False
 
-            # Also save this array as penval.npy
-            np.save('penval', thearray)
-            return None
+    @staticmethod
+    def show_stack(frame, mask):
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        stacked = np.hstack((mask_3, frame, res))
+        cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.4, fy=0.4))
 
 
 if __name__ == "__main__":
     capture = VideoCapture()
-    for x in range(10_000):
-        capture.loop()
-    capture.cap.read()
+    # for _ in range(10_000):
+    #     result = capture.calibrator(draw_rectangle=True)
+    #     if result:
+    #         break
+    c = None
+    x, y = 0, 0
+    for _ in range(10_000):
+        c, x, y = capture.draw_canvas(c, x, y)
     cv2.destroyAllWindows()
--- calibrator/clean.py
+++ calibrator/clean.py
@@ -4,8 +4,9 @@ import time
 
 
 class Cleaner:
-    def __init__(self, penval_file='penval.npy'):
+    def __init__(self, penval_file='penval.npy', noise=800):
         self.penval = np.load(penval_file)
+        self.noise = noise
         self.cap = cv2.VideoCapture(0)
         self.cap.set(3, 1280)
         self.cap.set(4, 720)
@@ -17,30 +18,39 @@ class Cleaner:
             return None
         frame = cv2.flip(frame, 1)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
         lower_range = self.penval[0]
         upper_range = self.penval[1]
 
-        mask = cv2.inRange(hsv, lower_range, upper_range)
 
-        mask = cv2.erode(mask, self.kernel, iterations=1)
-        mask = cv2.dilate(mask, self.kernel, iterations=2)
+        self.show_stack(frame, mask)
 
-        res = cv2.bitwise_and(frame, frame, mask=mask)
+        # contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+        #                                        cv2.CHAIN_APPROX_SIMPLE)
+        # if contours and cv2.contourArea(max(contours,
+        #                                     key=cv2.contourArea)) > self.noise:
+        #     # Grab the biggest contour with respect to area
+        #     c = max(contours, key=cv2.contourArea)
+        #
+        #     # Get bounding box coordinates around that contour
+        #     x, y, w, h = cv2.boundingRect(c)
+        #
+        #     # Draw that bounding box
+        #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 25, 255), 2)
+        #
+        # cv2.imshow('image', frame)
+        # # ESC key
+        # k = cv2.waitKey(5) & 0xFF
+        # if k == 27:
+        #     return True
 
-        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
-        # stack all frames and show it
-        stacked = np.hstack((mask_3, frame, res))
-        cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.4, fy=0.4))
-
-        k = cv2.waitKey(5) & 0xFF
-        if k == 27:
-            return None
 
 
 if __name__ == "__main__":
     capture = Cleaner()
     for x in range(10_000):
-        capture.loop()
-    capture.cap.read()
+        result = capture.loop()
+        if result:
+            break
     cv2.destroyAllWindows()
