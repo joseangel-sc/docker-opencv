2ea1484953b127b84795affcf80382f748d32a77
Jose Sanchez <jsanchezcastillejos@gmail.com>
2020-05-25 15:18:00 -0500

    Add assets folder

--- calibrator/calibrate.py
+++ calibrator/calibrate.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import cv2
 import numpy as np
@@ -13,12 +14,41 @@ class VideoCapture:
         self.noise = noise
         self.kernel = np.ones((5, 5), np.uint(8))
 
-    def draw_canvas(self, canvas, x1, y1):
+        width = int(self.cap.get(3))
+        height = int(self.cap.get(4))
+        self.out = cv2.VideoWriter('canvas.mp4', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (width, height))
+
+        self.pen, self.eraser, self.background_object, self.background_threshold = VideoCapture.switcher()
+        self.last_switch = time.time()
+        self.erase = False
+
+    @staticmethod
+    def switcher():
+        pen = cv2.resize(cv2.imread('assets/pen.png', 1), (50, 50))
+        eraser = cv2.resize(cv2.imread('assets/eraser.jpg', 1), (50, 50))
+        background_object = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+        background_threshold = 600
+        return pen, eraser, background_object, background_threshold
+
+    def draw_or_erase(self, frame):
+        top_left = frame[0:50, 0:50]
+        fgmask = self.background_object.apply(top_left)
+        switch_thresh = np.sum(fgmask == 255)
+        if switch_thresh > self.background_threshold and (time.time() - self.last_switch) > 1:
+            self.last_switch = time.time()
+            if not self.erase:
+                self.erase = True
+            else:
+                self.erase = False
+
+    def draw_canvas(self, canvas, x1, y1, record=True):
         clear = False
         wiper_thresh = 10000
         penval = np.load('penval.npy')
         _, frame = self.cap.read()
         frame = cv2.flip(frame, 1)
+        self.draw_or_erase(frame)
+
         if canvas is None:
             canvas = np.zeros_like(frame)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -34,7 +64,10 @@ class VideoCapture:
 
             k = cv2.waitKey(1) & 0xFF
             if (x1 != 0 or y1 != 0) and k != ord('s'):
-                canvas = cv2.line(canvas, (x1, y1), (x2, y2), [255, 204, 0], 8)
+                if not self.erase:
+                    canvas = cv2.line(canvas, (x1, y1), (x2, y2), [255, 204, 0], 8)
+                else:
+                    cv2.circle(canvas, (x2, y2), 50, (0, 0, 0), -1)
             x1, y1 = x2, y2
             if area > wiper_thresh:
                 cv2.putText(canvas, 'Clearing Canvas', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5,
@@ -44,13 +77,24 @@ class VideoCapture:
             x1, y1 = 0, 0
 
         frame = cv2.add(frame, canvas)
+        _, mask = cv2.threshold(cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY), 20, 255, cv2.THRESH_BINARY)
+        foreground = cv2.bitwise_and(canvas, canvas, mask=mask)
+        background = cv2.bitwise_and(frame, frame, mask=cv2.bitwise_not(mask))
+        frame = cv2.add(foreground, background)
 
-        stacked = np.hstack((canvas, frame))
-        cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.6, fy=0.6))
+        if self.erase:
+            cv2.circle(frame, (x1, y1), 20, (255, 255, 255), -1)
+            frame[0:50, 0:50] = self.eraser
+        else:
+            frame[0:50, 0:50] = self.pen
+        cv2.imshow('Air draw', frame)
+        if record:
+            self.out.write(frame)
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
+            if record:
+                self.out.release()
             sys.exit(0)
-        # When c is pressed clear the canvas
         if k == ord('c') or clear:
             canvas = None
         return canvas, x1, y1
@@ -87,7 +131,7 @@ class VideoCapture:
                                             key=cv2.contourArea)) > self.noise:
             co = max(contours, key=cv2.contourArea)
             x1, y1, w, h = cv2.boundingRect(co)
-            cv2.rectangle(frame, (x1, y1), (x + w, y1 + h), (0, 25, 255), 2)
+            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 25, 255), 2)
         cv2.imshow('image', frame)
 
     @staticmethod
@@ -132,12 +176,12 @@ class VideoCapture:
 
 if __name__ == "__main__":
     capture = VideoCapture()
-    for _ in range(10_000):
-        result = capture.calibrator(draw_rectangle=True)
-        if result:
-            break
+    # for _ in range(10_000):
+    #     result = capture.calibrator(draw_rectangle=True)
+    #     if result:
+    #         break
     c = None
     x, y = 0, 0
     for _ in range(10_000):
-        c, x, y = capture.draw_canvas(c, x, y)
+        c, x, y = capture.draw_canvas(c, x, y, record=True)
     cv2.destroyAllWindows()
